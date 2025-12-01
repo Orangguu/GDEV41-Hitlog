10bc2a8d771e2764d5d68c5bff00302e29b4e9b6
Chesca <fdjr401@gmail.com>
2025-12-01 15:25:39 +0800

    Refactor globals.hpp to have correct indentation size

--- globals.hpp
+++ globals.hpp
@@ -2,7 +2,7 @@
 #include "SceneManager.hpp"
 
 namespace Globals {
-  extern int WINDOW_WIDTH;
-  extern int WINDOW_HEIGHT;
-  extern SceneManager* sceneManager;
+    extern int WINDOW_WIDTH;
+    extern int WINDOW_HEIGHT;
+    extern SceneManager* sceneManager;
 };
